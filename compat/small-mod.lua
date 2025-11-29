c69a3a83b0caaad28b0f4ecbd57f0a1d18785842
ManaFriendsmith <42ah42@gmail.com>
2025-11-29 12:56:35 -0500

    space trains compat

---
+++ compat/small-mod.lua
@@ -0,0 +1,11 @@
+local rm = require("__pf-functions__/recipe-manipulation")
+local tm = require("__pf-functions__/technology-manipulation")
+local misc = require("__pf-functions__/misc")
+
+if mods["se-space-trains"] then
+    if misc.difficulty > 1 then
+        rm.AddIngredient("space-locomotive", "gyro", 10)
+        rm.AddIngredient("space-cargo-wagon", "gyro", 10)
+        rm.AddIngredient("space-fluid-wagon", "gyro", 10)
+    end
+end
